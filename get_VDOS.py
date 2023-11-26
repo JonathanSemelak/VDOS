b1e17f46e0c57a48d66f360b85cb81e2fe4269ef
Jota <jota.smk@gmail.com>
2023-11-26 04:56:57 -0300

    improve error handeling

--- get_VDOS.py
+++ get_VDOS.py
@@ -1,49 +1,40 @@
-#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-# Get VDOS (Vibrational Density of States) from Molecular Dynamics Trajectories
-#
-# ## Overview
-# This Python script, `get_VDOS.py`, is designed to calculate the Vibrational Density of States (VDOS)
-# from molecular dynamics trajectory files. It supports different file formats and offers flexibility
-# in terms of data processing and analysis.
-#
-# ## Features
-# - Supports XYZ and NETCDF file formats.
-# - Option to choose between full and bond-specific modes.
-# - Various windowing functions for Fourier Transform: Gaussian, Blackman-Harris, Hamming, Hann.
-# - Option for numerical or file-based velocity calculations.
-# - Autocorrelation Function (ACF) calculation and FFT for VDOS computation.
-#
-# ## Requirements
-# - Python 3
-# - Libraries: NumPy, SciPy, ASE (for XYZ files)
-# - A trajectory file in either XYZ or NETCDF format.
-# ## Usage
-# Run the script from the command line with the required arguments.
-#
-# python get_VDOS.py -i [input file] -o [output file] -dt [delta time] [other optional arguments]
-#
-# ## Command-Line Arguments
-# - `-i`, `--input`: Input file name (required).
-# - `-o`, `--output`: Output file name (required).
-# - `-m`, `--mode`: Mode of operation ('full' or 'bond', default: 'full').
-# - `-dt`, `--delta_t`: Delta time in femtoseconds (required).
-# - `-b`, `--bond`: Bond indices (two integers, required if mode is 'bond').
-# - `-w`, `--window_kind`: Window kind for FFT (default: 'Gaussian').
-# - `-n`, `--force_numerical`: Force numerical calculation of velocities (default: False).
-#
-# ## Examples
-# python get_VDOS.py -i trajectory.xyz -o vdos_output.txt -dt 0.5
-#
-# ## Acknowledgments
-# This project incorporates modified functions from [Velocity-ACF](https://github.com/LePingKYXK/Velocity-ACF)
-# by [LePingKYXK](https://github.com/LePingKYXK). We thank the author for their valuable work which formed a
-# foundation for some of the features in this project.
-#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
 import argparse
 import sys
 import math
 
+script_description = """
+------------------------------------------------------------------------------------------------------------
+Get VDOS (Vibrational Density of States) from Molecular Dynamics Trajectories
+
+## Overview
+This Python script, `get_VDOS.py`, is designed to calculate the Vibrational Density of States (VDOS)
+from molecular dynamics trajectory files. It supports different file formats and offers flexibility
+in terms of data processing and analysis.
+
+## Features
+- Supports XYZ and NETCDF file formats.
+- Option to choose between full and bond-specific modes.
+- Various windowing functions for Fourier Transform: Gaussian, Blackman-Harris, Hamming, Hann.
+- Option for numerical or file-based velocity calculations.
+- Autocorrelation Function (ACF) calculation and FFT for VDOS computation.
+
+## Requirements
+- Python 3
+- Libraries: NumPy, SciPy, ASE (for XYZ files)
+- A trajectory file in either XYZ or NETCDF format.
+
+## Usage
+Run the script from the command line with the required arguments.
+python get_VDOS.py -i [input file] -o [output file] -dt [delta time] [other optional arguments]
+
+## Acknowledgments
+This project incorporates modified functions from [Velocity-ACF](https://github.com/LePingKYXK/Velocity-ACF)
+by [LePingKYXK](https://github.com/LePingKYXK). We thank the author for their valuable work which formed a
+foundation for some of the features in this project.
+------------------------------------------------------------------------------------------------------------
+"""
+
+# Universal constants
 c = 2.9979245899e10 # speed of light in vacuum in [cm/s], from Wikipedia.
 # ------------------------------------------
 # Functions definition
@@ -59,6 +50,10 @@ def check_mode(args):
 def check_window_kind(window_kind):
     if window_kind not in ['Gaussian','Blackman-Harris','Hamming','Hann']:
         sys.exit("Error: Window kind for FFT (-w) must be 'Gaussian' or 'Blackman-Harris' or 'Hamming' or 'Hann'")
+
+def check_bool(inputvariable,rightvariable):
+    if inputvariable not in ['True','False']:
+        sys.exit("Error: "+ rightvariable + " must be 'True' or 'False'")
 
 def check_libraries_and_file(input_name):
     # Check and import common libraries
@@ -78,8 +73,10 @@ def check_libraries_and_file(input_name):
         except ImportError:
             sys.exit("Error: ASE library is required for XYZ files but is not installed.")
     elif input_name.endswith(('.nc', '.netcdf', '.crd')):
-        # Assuming no extra libraries are needed for NetCDF files
-        pass
+        try:
+            from scipy import signal
+        except ImportError:
+            sys.exit("Error: NETCDF_FILE from SCIPY.IO library is required but is not installed.")
     else:
         sys.exit(f"Error: Unsupported file format '{input_name}'.")
 # ------------------------------------------
@@ -141,7 +138,7 @@ def calc_ACF(array_1D):
     return autocor
 #-------------------------------------------
 # Create the parser
-parser = argparse.ArgumentParser(description='This gut will get the VDOS out of your trajectory file.')
+parser = argparse.ArgumentParser(description=script_description,formatter_class=argparse.RawDescriptionHelpFormatter)
 # Define the command-line arguments
 parser.add_argument('-i', '--input', required=True, help='Input file name')
 parser.add_argument('-o', '--output', required=True, help='Output file name')
@@ -149,7 +146,13 @@ parser.add_argument('-m', '--mode', default='full', help="Mode of operation ('fu
 parser.add_argument('-dt', '--delta_t', type=float, required=True, help='Delta time in femtoseconds')
 parser.add_argument('-b', '--bond', nargs=2, type=int, help='Bond indices (two integers)')
 parser.add_argument('-w', '--window_kind', default='Gaussian', help="Window kind for FFT ('Gaussian' or 'Blackman-Harris' or 'Hamming' or 'Hann')")
-parser.add_argument('-n', '--force_numerical', type=bool, default=False, help="Force numerical calculation of velocities")
+parser.add_argument('-f', '--force_numerical', default='False', help="Force numerical calculation of velocities")
+parser.add_argument('-n', '--use_normalized_vectors', default='False', help="Use normalized vectors")
+
+# If no arguments are provided, print the description and exit
+if len(sys.argv) == 1:
+    parser.print_help()
+    sys.exit()
 
 # Parse the arguments
 args = parser.parse_args()
@@ -161,14 +164,18 @@ mode = args.mode
 delta_t = args.delta_t * 1e-15  # Convert from femtoseconds to seconds
 bond_indices = args.bond if mode == 'bond' else None
 window_kind = args.window_kind
-force_numerical = args.force_numerical
 # Checks input_name extension and required libraries are installed
 check_libraries_and_file(input_name)
 # Checks the mode is an available option
 check_mode(args)
 # Checks the window_kind is an available option
 check_window_kind(window_kind)
-# Checks the force_numerical is an available option
+# Checks the variables are str True or False before converting to bool
+check_bool(args.force_numerical,"--force_numerical ( -f)")
+force_numerical = args.force_numerical == "True"
+# Checks the variables are str True or False before converting to bool
+check_bool(args.use_normalized_vectors,"--use_normalized_vectors ( -n)")
+force_numerical = args.use_normalized_vectors == "True"
 
 # Conditional import based on file extension
 if input_name.endswith('.xyz'): from ase.io import read
@@ -194,9 +201,6 @@ if input_name.endswith('.xyz'):  # XYZ file case
         print("The VDOS will be obtained considering all atoms")
         print("Velocities will be calculated numerically")
         normal_vectors = np.linalg.norm(coordinates, axis=-1)
-        print("nsteps,natoms=",nsteps,natoms)
-        print("coordinates shape=",np.shape(coordinates))
-        print("coordinates first",coordinates[0])
 else: # NETCDF file case
     print("Coordinates/velocities from the netcdf file will be read using the scipy library")
     print("Reading file...")
@@ -217,9 +221,6 @@ else: # NETCDF file case
             coordinates = np.array(trajectory.variables['coordinates'].data)
             nsteps = len(coordinates)
             natoms = len(coordinates[0])
-            print("nsteps,natoms=",nsteps,natoms)
-            print("coordinates shape=",np.shape(coordinates))
-            print("coordinates first",coordinates[0])
             normal_vectors = np.linalg.norm(coordinates, axis=-1)
         print("The program will deal with all atoms one by one.")
 
