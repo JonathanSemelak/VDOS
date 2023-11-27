c24eae282e3f4101bd8c29aacaed5bce47e99efa
Jota <jota.smk@gmail.com>
2023-11-27 14:29:41 -0300

    debug

--- get_VDOS.py
+++ get_VDOS.py
@@ -236,6 +236,7 @@ else: # NETCDF file case
         print("The VDOS associated with the stretching of two atoms will be obtained")
         print("derivatives will be calculated numerically")
         coordinates = np.array(trajectory.variables['coordinates'].data)
+        nsteps = len(coordinates)
         distances = np.linalg.norm((coordinates[:,bond_indices[0],:] - coordinates[:,bond_indices[1],:]), axis=1)
 
 window=choose_window(nsteps,window_kind)
